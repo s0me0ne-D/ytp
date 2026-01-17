000e71739b8f94194cb4aadf0c8716a3ef4a232a
s0me0ne-D <zdybeldenys@gmail.com>
2026-01-17 16:35:53 +0100

    first commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,18 +1,71 @@
-import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import { defineConfig } from 'eslint/config';
+import nextVitals from 'eslint-config-next/core-web-vitals';
+import nextTs from 'eslint-config-next/typescript';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+function stripPlugins(configs, pluginsToStrip) {
+  return configs.map((config) => {
+    if (config.plugins) {
+      const newPlugins = { ...config.plugins };
+      pluginsToStrip.forEach((pluginName) => {
+        delete newPlugins[pluginName];
+      });
+      return { ...config, plugins: newPlugins };
+    }
+    return config;
+  });
+}
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
-  globalIgnores([
-    // Default ignores of eslint-config-next:
-    ".next/**",
-    "out/**",
-    "build/**",
-    "next-env.d.ts",
+  ...stripPlugins(compat.extends('airbnb'), ['react', 'react-hooks', 'jsx-a11y']),
+  ...stripPlugins(compat.extends('airbnb-typescript'), [
+    'react',
+    'react-hooks',
+    'jsx-a11y',
+    '@typescript-eslint',
   ]),
+  ...compat.extends('plugin:prettier/recommended'),
+
+  {
+    languageOptions: {
+      parserOptions: {
+        project: './tsconfig.eslint.json',
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: {
+          project: './tsconfig.eslint.json',
+        },
+      },
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/jsx-props-no-spreading': 'off',
+      'react/require-default-props': 'off',
+      'import/prefer-default-export': 'off',
+      'import/extensions': 'off',
+      'react/function-component-definition': 'off',
+      '@typescript-eslint/no-unused-vars': 'error',
+      '@typescript-eslint/lines-between-class-members': 'off',
+      '@typescript-eslint/no-throw-literal': 'off',
+      'prettier/prettier': 'warn',
+    },
+  },
+  {
+    ignores: ['eslint.config.mjs', '.next/**', 'out/**', 'build/**', 'next-env.d.ts'],
+  },
 ]);
 
 export default eslintConfig;
